cbfd990712e8f111e66557e0e319fe7cebd88e1b
bijeshregmi <101452854+bijesh-regmi@users.noreply.github.com>
2025-12-22 14:29:45 +0545

    Add: thumbnail generation with cloudinary and ffmpeg for fallback

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,19 +3,29 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { Video } from "../models/video.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {
+    generateThumbnailClkoudinary,
+    generateThumbnailFfmpeg
+} from "../utils/generateThumbnail.js";
+import isValidUrl from "../utils/isValidURL.js";
 
 export const uploadVideo = asyncHandler(async (req, res) => {
     const { title, description } = req.body;
 
-    if (!title) throw new ApiError(400, "Title is required");
+    if (!title.trim()) throw new ApiError(400, "Title is required");
     //get the local path of files
     const videoPath = req.files?.videoFile?.[0].path;
     const thumbnailPath = req.files?.thumbnail?.[0].path;
 
     if (!videoPath) throw new ApiError(400, "Video required");
-    if (!thumbnailPath) {
-        //generate thumbnail using ffmpeg
-    }
+    // const video = await uploadOnCloudinary(videoPath);
+    // if (!thumbnailPath) {
+    //     //generate thumbnail using ffmpeg
+    //     let thumbnail = await generateThumbnailClkoudinary(video.public_id);
+    //     if (!thumbnail || !isValidUrl(thumbnail)) {
+    //         thumbnail = await generateThumbnailFfmpeg(video);
+    //     }
+    // }
 
     const [video, thumbnail] = await Promise.all([
         uploadOnCloudinary(videoPath),
@@ -40,8 +50,10 @@ export const uploadVideo = asyncHandler(async (req, res) => {
 
     const videoResponse = newVideo.toObject();
     delete videoResponse.publicId;
-    
+
     res.status(201).json(
         new ApiResponse(201, videoResponse, "Video upload successful!")
     );
 });
+
+export const getAllVideo = asyncHandler(async (req, res) => {});
